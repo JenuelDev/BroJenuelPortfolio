b81430d548750faf318f46a0b79e2c42989ad06d
Jenuel Oras Ganawed <jenuelganawed936@gmail.com>
2020-10-04 16:11:59 +0800

    Added A POwershell command to move my dist to my MisterJ936 folder for update

---
+++ dist.ps1
@@ -0,0 +1,12 @@
+if (Test-Path .\..\MisterJ936) {
+     Write-Host "Directory MisterJ936 Doesnt Exist"
+     if (Test-Path .\..\MisterJ936\dist) {
+          Remove-Item ".\..\MisterJ936\dist"
+     }
+     Copy-Item -Path ".\dist" -Destination ".\..\MisterJ936" -Recurse
+} else {
+     Write-Warning "Directory MisterJ936 Doesnt Exist"
+}
+
+
+Read-Host -Prompt "Press Enter to exit"
