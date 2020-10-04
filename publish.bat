2633a54f027b66a52e4964f998ed9e3dfc5631d6
Jenuel Oras Ganawed <jenuelganawed936@gmail.com>
2020-10-04 19:21:44 +0800

    Update Jenuel-portfolio-v2

---
+++ publish.bat
@@ -0,0 +1,11 @@
+git add .
+git commit -m "Update Jenuel-portfolio-v2"
+git push origin master
+cd ..
+cd misterj936
+git add .
+git commit -m "Update Portfolio"
+git push origin master
+cd ..
+cd jenuel-portfolio-v2
+pause
--- pusblish.bat
+++
@@ -1,8 +0,0 @@
-cd ..
-cd misterj836
-git add .
-git commit -m "Update Portfolio"
-git push origin
-cd ..
-cd jenuel-portfolio-v2
-pause
