282bcdb035e3a86bce40e341cd123c6740c07c51
Jenuel Oras Ganawed <jenuelganawed936@gmail.com>
2020-10-07 16:15:00 +0800

    Update Jenuel-portfolio-v2

--- dist.ps1
+++ dist.ps1
@@ -1,7 +1,9 @@
 if (Test-Path .\..\MisterJ936) {
      if (Test-Path .\..\MisterJ936\dist) {
+          Write-Host "Deleting Dist in MisterJ936..."
           Remove-Item ".\..\MisterJ936\dist" -Recurse -Force
      }
+     Write-Host "Copyint Dist to MisterJ936...."
      Copy-Item -Path ".\dist" -Destination ".\..\MisterJ936" -Recurse -Force
 } else {
      Write-Warning "Directory MisterJ936 Doesnt Exist"
