2e5eea0c011ffb950d50c506d410860b0dd04673
Jenuel Oras Ganawed <jenuelganawed936@gmail.com>
2020-10-07 14:35:36 +0800

    Update Jenuel-portfolio-v2

--- dist.ps1
+++ dist.ps1
@@ -1,6 +1,6 @@
 if (Test-Path .\..\MisterJ936) {
      if (Test-Path .\..\MisterJ936\dist) {
-          Remove-Item ".\..\MisterJ936\dist"
+          Remove-Item ".\..\MisterJ936\dist" -Recurse -Force
      }
      Copy-Item -Path ".\dist" -Destination ".\..\MisterJ936" -Recurse -Force
 } else {
