316cc4b98db8970bea372c75cc963f839b93ebb2
Jenuel Oras Ganawed <jenuelganawed936@gmail.com>
2020-10-07 13:13:31 +0800

    Update Jenuel-portfolio-v2

--- dist.ps1
+++ dist.ps1
@@ -1,7 +1,7 @@
 if (Test-Path .\..\MisterJ936) {
      if (Test-Path .\..\MisterJ936\dist) {
           Write-Warning "Removing Dist in MisterJ936..."
-          Remove-Item ".\..\MisterJ936\dist"  -Force -Recurse
+          Remove-Item ".\..\MisterJ936\dist"
      }
      Write-Warning "Copying Dist to MisterJ936..."
      Copy-Item -Path ".\dist" -Destination ".\..\MisterJ936"
