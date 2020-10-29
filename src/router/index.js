d8f526bd2b3158c027490df77a79503e1e404652
Jenuel Oras Ganawed <jenuelganawed936@gmail.com>
2020-10-29 10:29:47 +0800

    Update Jenuel-portfolio-v2

--- src/router/index.js
+++ src/router/index.js
@@ -6,49 +6,50 @@ import PageNotFound from '@/pages/page-not-found'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '',
-    redirect: '/intro'
-  },
-  {
-    path: '/intro',
-    name: 'Intro',
-    component: Intro
-  },
-  {
-    path: '/about',
-    name: "About",
-    component: () => import(/* webpackChunkName: "about" */ './../pages/about-me')
-  },
-  {
-    path: '/experience',
-    name: "Experience",
-    component: () => import(/* webpackChunkName: "experience" */ './../pages/my-experience')
-  },
-  {
-    path: '/work',
-    name: "Work",
-    component: () => import(/* webpackChunkName: "work" */ './../pages/my-work')
-  },
-  {
-    path: '/codechallenge',
-    name: "CodeChallenge",
-    component: () => import(/* webpackChunkName: "codechallenge" */ './../pages/code-challenge')
-  },
-  {
-    path: '/contact',
-    name: "Contact",
-    component: () => import(/* webpackChunkName: "contact" */ './../pages/contact-me')
-  },
-  {
-    path: '/*',
-    name: 'PageNotFound',
-    component: PageNotFound
-  }
+    {
+        path: '',
+        redirect: '/intro'
+    },
+    {
+        path: '/intro',
+        name: 'Intro',
+        component: Intro
+    },
+    {
+        path: '/about',
+        name: "About",
+        component: () => import(/* webpackChunkName: "about" */ './../pages/about-me')
+    },
+    {
+        path: '/experience',
+        name: "Experience",
+        component: () => import(/* webpackChunkName: "experience" */ './../pages/my-experience')
+    },
+    {
+        path: '/work',
+        name: "Work",
+        component: () => import(/* webpackChunkName: "work" */ './../pages/my-work')
+    },
+    {
+        path: '/codechallenge',
+        name: "CodeChallenge",
+        component: () => import(/* webpackChunkName: "codechallenge" */ './../pages/code-challenge')
+    },
+    {
+        path: '/contact',
+        name: "Contact",
+        component: () => import(/* webpackChunkName: "contact" */ './../pages/contact-me')
+    },
+    {
+        path: '/*',
+        name: 'PageNotFound',
+        component: PageNotFound
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    mode: "history",
+    routes
 })
 
 export default router
