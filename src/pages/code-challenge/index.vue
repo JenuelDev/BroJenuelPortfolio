682b5b0f0094848ffb2279b80819715e4066d63c
Jenuel Oras Ganawed <jenuelganawed936@gmail.com>
2020-11-07 10:37:20 +0800

    Update Jenuel-portfolio-v2

--- src/pages/code-challenge/index.vue
+++ src/pages/code-challenge/index.vue
@@ -26,18 +26,7 @@
                             <header>
                                 <div class="item-top">
                                     <div class="item-folder">
-                                        <svg
-                                            viewBox="0 0 15 15"
-                                            fill="none"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            width="40"
-                                            height="40"
-                                        >
-                                            <path
-                                                d="M.5 12.5v-10a1 1 0 011-1h4l2 2h6a1 1 0 011 1v8a1 1 0 01-1 1h-12a1 1 0 01-1-1z"
-                                                stroke="currentColor"
-                                            ></path>
-                                        </svg>
+                                        <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" width="40" height="40"><path d="M1.5 1A1.5 1.5 0 000 2.5v10A1.5 1.5 0 001.5 14h12a1.5 1.5 0 001.5-1.5v-8A1.5 1.5 0 0013.5 3H7.707l-2-2H1.5z" fill="currentColor"></path></svg>
                                     </div>
                                     <div class="item-links">
                                         <a
