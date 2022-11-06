e2bc485b4b4e8f62fd3db231bf7e75c9cd522f7c
Jenuel Ganawed <jenuelganawed936@gmail.com>
2022-11-06 15:41:29 +0800

    Updates

--- src/constant/code-challenges-page/challenges/index.js
+++ src/constant/code-challenges-page/challenges/index.js
@@ -1,8 +1,47 @@
-import letsChat from "./common/lets-chat";
-import portfolioDesign from "./common/portfolio-design";
-import speechToText from "./common/speech-to-text";
-import textToSpeech from "./common/text-to-speech";
-import webCalculator from "./common/web-calculator";
-import worldTimeReact from "./common/world-time-react";
+import { ExternalLinkSquareAlt, Github, Html5, NodeJs, Vuejs } from '@vicons/fa';
+import letsChat from './common/lets-chat';
+import portfolioDesign from './common/portfolio-design';
+import speechToText from './common/speech-to-text';
+import textToSpeech from './common/text-to-speech';
+import webCalculator from './common/web-calculator';
+import worldTimeReact from './common/world-time-react';
 
-export default [letsChat, worldTimeReact, portfolioDesign, webCalculator, speechToText, textToSpeech]
+export default [
+    {
+        title: 'Random Name Selector',
+        des: '<p>This is a simple name random name/item selector.</p>',
+        headIcon: 'chat',
+        links: [
+            {
+                link: 'https://github.com/BroJenuel/random-name-select',
+                icon: Github,
+                tooltip: 'Open In Github',
+            },
+            {
+                link: 'https://random-select.brojenuel.com',
+                icon: ExternalLinkSquareAlt,
+                tooltip: 'Open Link',
+            },
+        ],
+        techs: [
+            {
+                name: 'html&css',
+                icon: Html5,
+            },
+            {
+                name: 'express',
+                icon: NodeJs,
+            },
+            {
+                name: 'VueJS',
+                icon: Vuejs,
+            },
+        ],
+    },
+    letsChat,
+    worldTimeReact,
+    portfolioDesign,
+    webCalculator,
+    speechToText,
+    textToSpeech,
+];
--- src/constant/code-challenges-page/index.js
+++ src/constant/code-challenges-page/index.js
@@ -1,6 +1,6 @@
-import challenges from "./challenges";
+import challenges from './challenges';
 
 export default {
-    des: "This projects are for learning Purposes Only. You can check and see how I made them, you might learn from it too.",
-    challenges: challenges
-}
+    des: 'This projects are for learning Purposes Only. You can check and see how I made them, you might learn from it too.',
+    challenges: challenges,
+};
